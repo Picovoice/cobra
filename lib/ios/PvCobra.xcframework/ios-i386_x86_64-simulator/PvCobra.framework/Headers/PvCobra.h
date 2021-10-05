e5dc39de37e68a25003c13d75a8698d9cea35fde
Alireza Kenarsari <alireza@picovoice.ai>
2021-10-05 16:16:36 -0700

    ios

--- lib/ios/PvCobra.xcframework/ios-i386_x86_64-simulator/PvCobra.framework/Headers/PvCobra.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for PvCobra.
-FOUNDATION_EXPORT double PvCobraVersionNumber;
-
-//! Project version string for PvCobra.
-FOUNDATION_EXPORT const unsigned char PvCobraVersionString[];
