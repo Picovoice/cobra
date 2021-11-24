9ad2946c18d9a996621091c6a53d2ddc63cbff90
Pouya Rostam <pouya@picovoice.ai>
2021-11-24 11:26:24 -0800

    working 3

--- demo/ios/CobraDemo/CobraDemo/ViewModel.swift
+++ demo/ios/CobraDemo/CobraDemo/ViewModel.swift
@@ -13,8 +13,8 @@ import Combine
 
 class ViewModel: ObservableObject {
     
-     private let ACCESS_KEY = "{YOUR_ACCESS_KEY_HERE}"
-         
+    private let ACCESS_KEY = "{YOUR_ACCESS_KEY_HERE}"
+
     private let ALPHA: Float = 0.5
     
     private var cobra: Cobra!
