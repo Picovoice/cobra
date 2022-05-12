b2163499ba1c8819f59bb8777f112cacdf3ae6e2
Reza (Pouya) Rostam <42750891+mrrostam@users.noreply.github.com>
2022-05-12 15:02:03 -0700

    Python release

--- binding/python/setup.py
+++ binding/python/setup.py
@@ -55,7 +55,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvcobra",
-    version="1.1.2",
+    version="1.1.3",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Cobra voice activity detection (VAD) engine",
