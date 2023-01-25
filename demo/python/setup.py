52e7f0a3422bf5e53d63d483095dc359d4b608c3
Eric Mikulin <eric@picovoice.ai>
2023-01-25 13:44:33 -0800

    Address PR comments

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvcobrademo",
-    version="1.2.0",
+    version="1.2.1",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Cobra voice activity detection (VAD) engine demos.",
@@ -36,7 +36,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/cobra",
     packages=["pvcobrademo"],
-    install_requires=["pvcobra==1.1.3", "pvrecorder==1.1.0"],
+    install_requires=["pvcobra==1.2.0", "pvrecorder==1.1.1"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
