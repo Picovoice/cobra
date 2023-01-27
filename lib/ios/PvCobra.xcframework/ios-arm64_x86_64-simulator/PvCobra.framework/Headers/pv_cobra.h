6dfd09550bcf21c324fd90c0503f62aeacac10e9
Ian Lavery <ian@picovoice.ai>
2023-01-27 09:41:42 -0800

    v1.2 (#130)

--- lib/ios/PvCobra.xcframework/ios-arm64_x86_64-simulator/PvCobra.framework/Headers/pv_cobra.h
+++ lib/ios/PvCobra.xcframework/ios-arm64_x86_64-simulator/PvCobra.framework/Headers/pv_cobra.h
@@ -28,8 +28,8 @@ extern "C"
 /**
  * Forward declaration for Cobra voice activity detection (VAD) engine. It detects speech signals within an incoming
  * stream of audio in real-time. It processes incoming audio in consecutive frames and for each frame emits the
- * probability of voice activity. The number of samples per frame can be attained by calling 'pv_cobra_frame_length()'.
- * The incoming audio needs to have a sample rate equal to 'pv_sample_rate()' and be 16-bit linearly-encoded. Cobra
+ * probability of voice activity. The number of samples per frame can be attained by calling `pv_cobra_frame_length()`.
+ * The incoming audio needs to have a sample rate equal to `pv_sample_rate()` and be 16-bit linearly-encoded. Cobra
  * operates on single-channel audio.
  */
 typedef struct pv_cobra pv_cobra_t;
@@ -42,7 +42,7 @@ typedef struct pv_cobra pv_cobra_t;
  * @param memory_size Memory size in bytes.
  * @param memory_buffer Memory buffer needs to be 8-byte aligned.
  * @param[out] object Constructed instance of Cobra.
- * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT' or 'PV_STATUS_OUT_OF_MEMORY' on failure.
+ * @return Status code. Returns `PV_STATUS_INVALID_ARGUMENT` or `PV_STATUS_OUT_OF_MEMORY` on failure.
  */
 PV_API pv_status_t pv_cobra_init(int32_t memory_size, void *memory_buffer, pv_cobra_t **object);
 
@@ -51,11 +51,11 @@ PV_API pv_status_t pv_cobra_init(int32_t memory_size, void *memory_buffer, pv_co
 /**
  * Constructor.
  *
- * @param access_key AccessKey provided by Picovoice Console (https://picovoice.ai/console/)
+ * @param access_key AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)
  * @param object Constructed instance of Cobra.
- * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT' or 'PV_STATUS_OUT_OF_MEMORY',
- * 'PV_STATUS_RUNTIME_ERROR', 'PV_STATUS_ACTIVATION_ERROR', 'PV_STATUS_ACTIVATION_LIMIT_REACHED',
- * 'PV_STATUS_ACTIVATION_THROTTLED', or 'PV_STATUS_ACTIVATION_REFUSED' on failure.
+ * @return Status code. Returns `PV_STATUS_INVALID_ARGUMENT` or `PV_STATUS_OUT_OF_MEMORY`,
+ * `PV_STATUS_RUNTIME_ERROR`, `PV_STATUS_ACTIVATION_ERROR`, `PV_STATUS_ACTIVATION_LIMIT_REACHED`,
+ * `PV_STATUS_ACTIVATION_THROTTLED`, or `PV_STATUS_ACTIVATION_REFUSED` on failure.
  */
 PV_API pv_status_t pv_cobra_init(const char *access_key, pv_cobra_t **object);
 
@@ -73,10 +73,10 @@ PV_API void pv_cobra_delete(pv_cobra_t *object);
  *
  * @param object Cobra object.
  * @param pcm A frame of audio samples. The number of samples per frame can be attained by calling
- * 'pv_cobra_frame_length()'. The incoming audio needs to have a sample rate equal to 'pv_sample_rate()' and be 16-bit
+ * `pv_cobra_frame_length()`. The incoming audio needs to have a sample rate equal to `pv_sample_rate()` and be 16-bit
  * linearly-encoded. Cobra operates on single-channel audio.
  * @param[out] is_voiced Probability of voice activity. It is a floating-point number within [0, 1].
- * @return Returns 'PV_STATUS_INVALID_ARGUMENT' or 'PV_STATUS_OUT_OF_MEMORY' on failure.
+ * @return Returns `PV_STATUS_INVALID_ARGUMENT` or `PV_STATUS_OUT_OF_MEMORY` on failure.
  */
 PV_API pv_status_t pv_cobra_process(pv_cobra_t *object, const int16_t *pcm, float *is_voiced);
 
