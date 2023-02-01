2562fa16edc0c5bdd6dffa56ba8bd80de534e6cc
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-02-01 10:26:29 -0800

    Upadated web binding/demo (#131)
    * add pv error
    
    * release + demo

--- binding/web/src/cobra.ts
+++ binding/web/src/cobra.ts
@@ -1,5 +1,5 @@
 /*
-  Copyright 2022 Picovoice Inc.
+  Copyright 2022-2023 Picovoice Inc.
 
   You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
   file accompanying this source.
@@ -19,6 +19,7 @@ import {
   buildWasm,
   isAccessKeyValid,
   pv_free_type,
+  PvError
 } from '@picovoice/web-utils';
 
 import { simd } from 'wasm-feature-detect';
@@ -305,7 +306,9 @@ export class Cobra {
 
     const memoryBufferUint8 = new Uint8Array(memory.buffer);
 
-    const exports = await buildWasm(memory, wasmBase64);
+    const pvError = new PvError();
+
+    const exports = await buildWasm(memory, wasmBase64, pvError);
 
     const aligned_alloc = exports.aligned_alloc as aligned_alloc_type;
     const pv_free = exports.pv_free as pv_free_type;
@@ -351,11 +354,13 @@ export class Cobra {
 
     const status = await pv_cobra_init(accessKeyAddress, objectAddressAddress);
     if (status !== PV_STATUS_SUCCESS) {
+      const msg = `'pv_cobra_init' failed with status ${arrayBufferToStringAtIndex(
+        memoryBufferUint8,
+        await pv_status_to_string(status)
+      )}`
+
       throw new Error(
-        `'pv_cobra_init' failed with status ${arrayBufferToStringAtIndex(
-          memoryBufferUint8,
-          await pv_status_to_string(status)
-        )}`
+        `${msg}\nDetails: ${pvError.getErrorString()}`
       );
     }
     const memoryBufferView = new DataView(memory.buffer);
