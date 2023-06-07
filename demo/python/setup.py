1b4c0c8b13a4336cddf1a88f96330973b1327911
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-06-07 15:03:05 -0700

    python release

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="pvcobrademo",
-    version="1.2.2",
+    version="1.2.3",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Cobra voice activity detection (VAD) engine demos.",
@@ -36,7 +36,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/cobra",
     packages=["pvcobrademo"],
-    install_requires=["pvcobra==1.2.1", "pvrecorder==1.1.1"],
+    install_requires=["pvcobra==1.2.2", "pvrecorder==1.1.1"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
