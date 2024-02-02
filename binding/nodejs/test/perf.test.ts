945e36fff78de448db19dc68a373c313c846c27c
Albert Ho <albert@picovoice.ai>
2024-02-02 11:50:17 -0800

    rm init test

--- binding/nodejs/test/perf.test.ts
+++ binding/nodejs/test/perf.test.ts
@@ -28,11 +28,6 @@ const NUM_TEST_ITERATIONS = Number(
     .filter(x => x.startsWith('--num_test_iterations='))[0]
     ?.split('--num_test_iterations=')[1] ?? 0
 );
-const INIT_PERFORMANCE_THRESHOLD_SEC = Number(
-  process.argv
-    .filter(x => x.startsWith('--init_performance_threshold_sec='))[0]
-    ?.split('--init_performance_threshold_sec=')[1] ?? 0
-);
 const PROC_PERFORMANCE_THRESHOLD_SEC = Number(
   process.argv
     .filter(x => x.startsWith('--proc_performance_threshold_sec='))[0]
@@ -40,27 +35,6 @@ const PROC_PERFORMANCE_THRESHOLD_SEC = Number(
 );
 
 describe('Performance', () => {
-  test('init performance', () => {
-    let perfResults = [];
-    for (let i = 0; i < NUM_TEST_ITERATIONS; i++) {
-      const before = performance.now();
-      let cobraEngine = new Cobra(ACCESS_KEY);
-      let initTime = performance.now() - before;
-
-      cobraEngine.release();
-
-      if (i > 0) {
-        perfResults.push(initTime);
-      }
-    }
-    let avgPerfMs =
-      perfResults.reduce((acc, a) => acc + a, 0) / NUM_TEST_ITERATIONS;
-    let avgPerfSec = Number((avgPerfMs / 1000).toFixed(3));
-    // eslint-disable-next-line no-console
-    console.log('Average init performance: ' + avgPerfSec);
-    expect(avgPerfSec).toBeLessThanOrEqual(INIT_PERFORMANCE_THRESHOLD_SEC);
-  });
-
   test('proc performance', () => {
     let cobraEngine = new Cobra(ACCESS_KEY);
 
