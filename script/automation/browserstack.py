34a7b90b25c97af2886d1a3bb8147f5ea6940e69
Albert Ho <albert@picovoice.ai>
2025-09-25 14:13:04 -0700

    update ios device

--- script/automation/browserstack.py
+++ script/automation/browserstack.py
@@ -17,7 +17,7 @@ devices_dict = {
         'Google Pixel 6 Pro-15.0'
     ],
     'ios-min-max': [
-        'iPhone SE 2020-13',
+        'iPhone 12-15',
         'iPhone 14 Pro-16',
         'iPhone 14-18'
     ],
