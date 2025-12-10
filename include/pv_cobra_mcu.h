d1d48a172b3f4c7d921d7acc6a1f3117e5d0a802
Zhuojin Liu <liu@picovoice.ai>
2025-12-10 14:32:11 -0800

    update

--- include/pv_cobra_mcu.h
+++ include/pv_cobra_mcu.h
@@ -77,6 +77,30 @@ PV_API int32_t pv_cobra_frame_length(void);
  */
 PV_API const char *pv_cobra_version(void);
 
+/**
+ * Gets a list of hardware devices that can be specified when calling `pv_cobra_init`
+ *
+ * @param[out] hardware_devices Array of available hardware devices. Devices are NULL terminated strings.
+ *                              The array must be freed using `pv_cobra_free_hardware_devices`.
+ * @param[out] num_hardware_devices The number of devices in the `hardware_devices` array.
+ * @return Status code. Returns `PV_STATUS_OUT_OF_MEMORY`, `PV_STATUS_INVALID_ARGUMENT`, `PV_STATUS_INVALID_STATE`,
+ * `PV_STATUS_RUNTIME_ERROR`, `PV_STATUS_ACTIVATION_ERROR`, `PV_STATUS_ACTIVATION_LIMIT_REACHED`,
+ * `PV_STATUS_ACTIVATION_THROTTLED`, or `PV_STATUS_ACTIVATION_REFUSED` on failure.
+ */
+PV_API pv_status_t pv_cobra_list_hardware_devices(
+        char ***hardware_devices,
+        int32_t *num_hardware_devices);
+
+/**
+ * Frees memory allocated by `pv_cobra_list_hardware_devices`.
+ *
+ * @param[out] hardware_devices Array of available hardware devices allocated by `pv_cobra_list_hardware_devices`.
+ * @param[out] num_hardware_devices The number of devices in the `hardware_devices` array.
+ */
+PV_API void pv_cobra_free_hardware_devices(
+        char **hardware_devices,
+        int32_t num_hardware_devices);
+
 #ifdef __cplusplus
 }
 
