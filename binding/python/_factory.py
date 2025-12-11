6e0728787dff9d6819bf873476db249c7014340f
Zhuojin Liu <liu@picovoice.ai>
2025-12-11 13:25:50 -0800

    fix

--- binding/python/_factory.py
+++ binding/python/_factory.py
@@ -9,7 +9,7 @@
 # specific language governing permissions and limitations under the License.
 #
 
-from typing import Optional
+from typing import Optional, Sequence
 
 from ._cobra import *
 from ._util import *
