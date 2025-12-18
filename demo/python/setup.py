2faf3840082aeeb030cdf85eec0f1ea44c1015c6
matt200-ok <matthew@picovoice.ai>
2025-12-18 13:26:01 -0800

    v3.0.1 desktop (#287)

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -31,7 +31,7 @@ with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f
 
 setuptools.setup(
     name="pvcobrademo",
-    version="3.0.0",
+    version="3.0.1",
     author="Picovoice",
     author_email="hello@picovoice.ai",
     description="Cobra voice activity detection (VAD) engine demos.",
