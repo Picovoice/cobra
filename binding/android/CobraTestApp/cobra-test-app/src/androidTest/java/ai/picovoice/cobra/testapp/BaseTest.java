4f9d4e26dc9d003e98de2a1bbaecdaac6fcdb815
Eric Mikulin <eric@picovoice.ai>
2026-06-26 14:31:48 -0700

    Test failure

--- binding/android/CobraTestApp/cobra-test-app/src/androidTest/java/ai/picovoice/cobra/testapp/BaseTest.java
+++ binding/android/CobraTestApp/cobra-test-app/src/androidTest/java/ai/picovoice/cobra/testapp/BaseTest.java
@@ -108,7 +108,7 @@ public class BaseTest {
         Arrays.fill(labels, 163, 183, 1.0f);
         Arrays.fill(labels, 227, 252, 1.0f);
 
-        assertEquals(labels.length, probs.size());
+        assertEquals(labels.length, probs.size() - 1);
 
         float error = 0.f;
 
